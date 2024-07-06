759d40d0610c12b95c91e0dafe0833c33d196c4d
Maxim Lemberg <practicum@lemberg.dev>
2024-07-07 01:31:18 +0700

    sprint 8

--- app/src/main/java/dev/lemberg/playlistmaker/MainActivity.kt
+++ app/src/main/java/dev/lemberg/playlistmaker/MainActivity.kt
@@ -1,20 +1,40 @@
 package dev.lemberg.playlistmaker
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+        val mediaButton = findViewById<Button>(R.id.media_button)
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        searchButton.setOnClickListener {
+
+            val displayIntent = Intent(this@MainActivity, SearchActivity::class.java)
+            startActivity(displayIntent)
+
+        }
+
+        mediaButton.setOnClickListener {
+
+            val displayIntent = Intent(this@MainActivity, MediaActivity::class.java)
+            startActivity(displayIntent)
+
+        }
+
+        settingsButton.setOnClickListener {
+
+            val displayIntent = Intent(this@MainActivity, SettingsActivity::class.java)
+            startActivity(displayIntent)
+
         }
     }
 }
