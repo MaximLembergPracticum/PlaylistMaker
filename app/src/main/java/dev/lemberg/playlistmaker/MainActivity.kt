cad51456377fefa097ad07a602d81a0aed0a1274
Maxim Lemberg <github@lemberg.dev>
2024-09-22 16:51:28 +0700

    fix

--- app/src/main/java/dev/lemberg/playlistmaker/MainActivity.kt
+++ app/src/main/java/dev/lemberg/playlistmaker/MainActivity.kt
@@ -12,10 +12,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val searchButton = findViewById<Button>(R.id.search_button)
-        val mediaButton = findViewById<Button>(R.id.media_button)
-        val settingsButton = findViewById<Button>(R.id.settings_button)
 
+        val searchButton = findViewById<Button>(R.id.search_button)
         searchButton.setOnClickListener {
 
             val displayIntent = Intent(this@MainActivity, SearchActivity::class.java)
@@ -23,6 +21,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        val mediaButton = findViewById<Button>(R.id.media_button)
         mediaButton.setOnClickListener {
 
             val displayIntent = Intent(this@MainActivity, MediaActivity::class.java)
@@ -30,6 +29,7 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        val settingsButton = findViewById<Button>(R.id.settings_button)
         settingsButton.setOnClickListener {
 
             val displayIntent = Intent(this@MainActivity, SettingsActivity::class.java)
